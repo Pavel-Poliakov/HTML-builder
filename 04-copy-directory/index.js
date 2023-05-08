cec73501647f8b676a094aa020ff1f8343c14073
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-05-08 22:02:39 +0400

    fix: made task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+const copyDir = path.join(__dirname, 'files');
+const newDir = path.join(__dirname, 'files-copy');
 
+const resFiles = (dir, newdir) => {
+  fs.readdir(newdir, (err, data) => {
+    data.forEach((el) => {
+      fs.unlink(path.join(newdir, el.toString()), (err) => {
+        console.log('успешно очищен');
+      });
+    });
+  });
+  fs.readdir(dir, (err, data) => {
+    data.forEach((el) => {
+      fs.stat(path.join(dir, el.toString()), (err, stats) => {
+        if (stats.isFile()) {
+          fs.copyFile(
+            path.join(dir, el.toString()),
+            path.join(newdir, el.toString()),
+            (err, data) => {
+              console.log('good copy');
+            },
+          );
+        }
+      });
+    });
+  });
+};
+resFiles(copyDir, newDir);

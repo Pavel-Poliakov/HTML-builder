c4ee0793e6042cbe9419c227d156f31b242e3d2a
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-05-09 17:10:17 +0400

    fix: added create folder and files

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -10,6 +10,8 @@ const styleFiles = path.join(__dirname, 'styles');
 const output = fs.createWriteStream(bundel);
 const outputIndex = fs.createWriteStream(index);
 const outputTemp = fs.createReadStream(path.join(__dirname, 'template.html'));
+const assetsFiles = path.join(__dirname, 'assets');
+
 const bundelCss = (bundel, styles) => {
   fs.readdir(styles, (err, data) => {
     data.forEach((el) => {
@@ -30,3 +32,30 @@ const createIndex = () => {
   outputTemp.pipe(outputIndex);
 };
 createIndex();
+
+const copyDir = (assets, copFiles) => {
+  const nameFolder = path.basename(assets);
+  fs.mkdir(path.join(copFiles, nameFolder.toString()), (err, data) => {});
+  fs.readdir(path.join(assets), (err, data) => {
+    data.forEach((el) => {
+      console.log(el);
+      fs.stat(path.join(assets, el.toString()), (err, stats) => {
+        if (stats.isDirectory()) {
+          const newAss = path.join(assets, el.toString());
+          const newCopy = path.join(copFiles, nameFolder.toString());
+          copyDir(newAss, newCopy);
+        }
+        if (stats.isFile()) {
+          fs.copyFile(
+            path.join(assets, el.toString()),
+            path.join(copFiles, nameFolder.toString(), el.toString()),
+            (err, data) => {
+              console.log('good copy');
+            },
+          );
+        }
+      });
+    });
+  });
+};
+copyDir(assetsFiles, projectDist);

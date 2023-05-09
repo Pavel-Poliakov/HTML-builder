b473de8129bccd5788183f6bb92b3f0266d583fe
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-05-09 12:14:19 +0400

    fix: create style and index

---
+++ 06-build-page/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'project-dist'), (err) => {});
+const projectDist = path.join(__dirname, 'project-dist');
+
+const bundel = path.join(__dirname, 'project-dist', 'style.css');
+const index = path.join(__dirname, 'project-dist', 'index.html');
+const styleFiles = path.join(__dirname, 'styles');
+const output = fs.createWriteStream(bundel);
+const outputIndex = fs.createWriteStream(index);
+const outputTemp = fs.createReadStream(path.join(__dirname, 'template.html'));
+const bundelCss = (bundel, styles) => {
+  fs.readdir(styles, (err, data) => {
+    data.forEach((el) => {
+      fs.stat(path.join(styles, el.toString()), (err, stats) => {
+        if (stats.isFile() && path.extname(el).toString() === '.css') {
+          const input = fs.createReadStream(path.join(styles, el.toString()));
+          input.on('data', (data) => {
+            bundel.write(data.toString() + '\n');
+          });
+        }
+      });
+    });
+  });
+};
+bundelCss(output, styleFiles);
+
+const createIndex = () => {
+  outputTemp.pipe(outputIndex);
+};
+createIndex();
